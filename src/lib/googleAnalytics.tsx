7155e9744b2e84239b92f4dacb9a642200cb57d1
Oscar Marti <oscarmartitech@gmail.com>
2025-11-08 17:13:23 +0100

    Set children for GA

--- src/lib/googleAnalytics.tsx
+++ src/lib/googleAnalytics.tsx
@@ -13,20 +13,16 @@ export default function GoogleAnalytics({ gaId }: GoogleAnalyticsProps) {
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
       />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gaId}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gaId}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   );
 }
